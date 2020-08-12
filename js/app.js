a8f349d4ca21747611bfb57f2f28b4865a4e196e
Murad Aghazada <muradaghazada@Murads-MacBook-Pro-2.local>
2020-08-12 18:19:14 +0400

    rating pop-up added

--- js/app.js
+++ js/app.js
@@ -10,3 +10,8 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
+
+// function pop_up() {
+//   var popup = document.querySelector("#myPopup");
+//   popup.classList.toggle("show");
+// }
